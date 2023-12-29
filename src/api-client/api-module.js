b10f7dfa92db70f35700bd2f7fb42d6738a9cd01
Soham Shinde <88761532+soham-shinde@users.noreply.github.com>
2023-12-29 18:04:08 +0000

    1

--- src/api-client/api-module.js
+++ src/api-client/api-module.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const url = 'http://127.0.0.1:8080';
+const url = 'https://fitness-stock.onrender.com';
 
 export async function verifyUser(email, password, type) {
     try {
