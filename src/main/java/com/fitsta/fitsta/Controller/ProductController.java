39dfd5040c03440987a97b4f88fe800c88a0f1e1
Soham Shinde <88761532+soham-shinde@users.noreply.github.com>
2023-12-29 19:11:02 +0000

    1

--- src/main/java/com/fitsta/fitsta/Controller/ProductController.java
+++ src/main/java/com/fitsta/fitsta/Controller/ProductController.java
@@ -25,7 +25,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import com.fitsta.fitsta.Component.Validation;
 import com.fitsta.fitsta.DTO.UpdateProductRequest;
 import com.fitsta.fitsta.Entity.Product;
